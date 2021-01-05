a31e598f74049f13140d12822deb53be811dfe62
Daniil_Denisov <cap.den2001@gmail.com>
2021-01-05 16:17:36 +0300

    Создание heder для dekstop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,34 @@
 import classes from './Header.module.scss'
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
+  const seachIconStyles = {
+    position: 'relative',
+    left: '30px'
+  }
+
   return (
     <div className={classes.Header}>
-      <h1>Header</h1>
+      <a href="/" className={classes.Header__logo}>D Y L</a>
+
+      <nav className={classes.navbar}>
+        <ul className={classes.navbar__list}>
+          <li className={classes.navbar__item}>
+            <a href="/" className={classes.navbar__link}>Лучшие</a>
+          </li>
+          <li className={classes.navbar__item}>
+            <a href="/" className={classes.navbar__link}>Прямые трансляции</a>
+          </li>
+          <li className={classes.navbar__item}>
+            <a href="/" className={classes.navbar__link}>Вакансии</a>
+          </li>
+        </ul>
+      </nav>
+      <SearchIcon color="action" style={seachIconStyles} />
+      <input className={classes.Header__search} placeholder="Поиск..."/>
+
+      <button className={classes.Header__signIn}>Войти</button>
+      <button className={classes.Header__signUp}>Регистрация</button>
     </div>
   );
 }
